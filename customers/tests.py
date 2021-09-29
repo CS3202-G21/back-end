a6acae33453e57226b7ca372846db851d53579ec
Dhaura Pathirana <dhaurapathirana@gmail.com>
2021-09-29 22:01:51 +0530

    customer unit test implemented

--- customers/tests.py
+++ customers/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+import json
 
-# Create your tests here.
+
+class CustomerUserTests(TestCase):
+    def Test_register_customer(self):
+        url = '/api/auth/customer/register'
+        data = {
+            "username": "johndoe",
+            "first_name": "John",
+            "last_name": "Doe",
+            "email": "jdoe@gmail.com",
+            "password": "123456"
+        }
+
+        client = Client()
+        response = client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def Test_login_customer(self):
+        url = '/api/auth/customer/login'
+        data = {
+            "username": "johndoe",
+            "password": "123456"
+        }
+
+        client = Client()
+        response = client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        return json.loads(response.content.decode("utf-8"))["token"]
+
+    def Test_get_user(self, token):
+        url = '/api/auth/customer/user'
+
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        response = client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_register_customer_then_login_customer(self):
+        self.Test_register_customer()
+        token = self.Test_login_customer()
+        self.Test_get_user(token)
--- customers/urls.py
+++ customers/urls.py
@@ -5,7 +5,7 @@ from knox import views as knox_views
 urlpatterns = [
     path('api/auth/customer', include('knox.urls')),
     path('api/auth/customer/register', RegisterCustomerAPI.as_view()),
-    path('api/auth/customer/login', LoginCustomerAPI.as_view()),
+    path('api/auth/customer/login', LoginCustomerAPI.as_view(), name='customer-login'),
     path('api/auth/customer/user', CustomerAPI.as_view()),
     path('api/auth/customer/logout', knox_views.LogoutView.as_view(), name='knox_logout')
 ]
