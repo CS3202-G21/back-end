27062c55cc43d36b3ee0207294a95483b687d5b8
Dhaura Pathirana <dhaurapathirana@gmail.com>
2021-09-29 23:43:34 +0530

    staff unit test implemented

--- staff/tests.py
+++ staff/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+import json
 
-# Create your tests here.
+
+class StaffUserTests(TestCase):
+    def Test_register_staff(self, group):
+        url = '/api/auth/staff/register_test'
+        data = {
+            "username": "testing",
+            "first_name": "Test",
+            "last_name": "Test",
+            "email": "test@gmail.com",
+            "password": "sep18g21",
+            "group": group
+        }
+
+        client = Client()
+        response = client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def Test_login_staff(self):
+        url = '/api/auth/staff/login'
+        data = {
+            "username": "testing",
+            "password": "sep18g21"
+        }
+
+        client = Client()
+        response = client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        return json.loads(response.content.decode("utf-8"))["token"]
+
+    def Test_get_user(self, token):
+        url = '/api/auth/staff/user'
+
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        response = client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_register_customer_then_login_customer(self):
+        self.Test_register_staff("Chef")
+        token = self.Test_login_staff()
+        self.Test_get_user(token)
--- staff/urls.py
+++ staff/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .api import LoginStaffAPI, StaffAPI
+from .api import LoginStaffAPI, StaffAPI, RegisterStaffAPI
 from knox import views as knox_views
 
 urlpatterns = [
     path('api/auth/staff', include('knox.urls')),
     path('api/auth/staff/login', LoginStaffAPI.as_view()),
     path('api/auth/staff/user', StaffAPI.as_view()),
-    path('api/auth/staff/logout', knox_views.LogoutView.as_view(), name='knox_logout')
+    path('api/auth/staff/logout', knox_views.LogoutView.as_view(), name='knox_logout'),
+    path('api/auth/staff/register_test', RegisterStaffAPI.as_view()),
 ]
