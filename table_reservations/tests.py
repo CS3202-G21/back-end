99aa9c7157bd73c1c8bc6431734a937bac30ef01
Dhaura Pathirana <dhaurapathirana@gmail.com>
2021-10-20 23:08:30 +0530

    unauthorized access tests added

--- table_reservations/tests.py
+++ table_reservations/tests.py
@@ -7,58 +7,42 @@ import json
 
 
 class TableReservationTests(TestCase):
-    def Test_create_table_reservation(self, token):
-        url = '/api/table_reservations'
-        data = {
-            "restaurant": RestaurantsTests().Test_create_restaurant(),
-            "meal_time": 2,
-            "reserved_date": str(datetime.now().date()),
-            "num_of_people": 4
-        }
-
-        client = APIClient()
-        client.credentials(HTTP_AUTHORIZATION='Token ' + token)
-        response = client.post(url, data, format='json')
+    def Test_success_create_table_reservation(self, token):
+        response = self.Test_create_table_reservation(token)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         return json.loads(response.content.decode("utf-8"))["table_reservation"]["id"]
 
-    def Test_update_customer_arrival(self, token, reservation_id):
-        url = '/api/table_reservations/arrival'
-        data = {
-            "reservation_id": reservation_id
-        }
-
-        client = APIClient()
-        client.credentials(HTTP_AUTHORIZATION='Token ' + token)
-        response = client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def Test_invalid_update_customer_arrival(self, token, reservation_id):
-        url = '/api/table_reservations/arrival'
-        data = {
-            "reservation_id": reservation_id
-        }
-
-        client = APIClient()
-        client.credentials(HTTP_AUTHORIZATION='Token ' + token)
-        response = client.post(url, data, format='json')
+    def Test_unauthorized_create_table_reservation(self, token):
+        response = self.Test_create_table_reservation(token)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def Test_view_table_reservations(self, token):
-        url = '/api/table_reservations/today_reservations'
-
-        client = APIClient()
-        client.credentials(HTTP_AUTHORIZATION='Token ' + token)
-        response = client.get(url)
+    def Test_success_update_customer_arrival(self, token, reservation_id):
+        response = self.Test_update_customer_arrival(token, reservation_id)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def Test_get_table_reservations(self, token):
-        url = '/api/table_reservations'
+    def Test_unauthorized_update_customer_arrival(self, token, reservation_id):
+        response = self.Test_update_customer_arrival(token, reservation_id)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-        client = APIClient()
-        client.credentials(HTTP_AUTHORIZATION='Token ' + token)
-        response = client.get(url)
+    def Test_invalid_update_customer_arrival(self, token, reservation_id):
+        response = self.Test_update_customer_arrival(token, reservation_id)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def Test_success_view_table_reservations(self, token):
+        response = self.Test_view_table_reservations(token)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def Test_unauthorized_view_table_reservations(self, token):
+        response = self.Test_view_table_reservations(token)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def Test_success_get_table_reservations(self, token):
+        response = self.Test_get_table_reservations(token)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def Test_unauthorized_get_table_reservations(self, token):
+        response = self.Test_get_table_reservations(token)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_table_reservation_process(self):
         customer_data = {
@@ -80,16 +64,106 @@ class TableReservationTests(TestCase):
         }
         waiter_token = self.register_user(waiter_data, '/api/auth/staff/register_test')
 
-        reservation_id = self.Test_create_table_reservation(customer_token)
+        reservation_id = self.Test_success_create_table_reservation(customer_token)
 
-        self.Test_view_table_reservations(waiter_token)
-
-        self.Test_update_customer_arrival(waiter_token, reservation_id)
-
-        self.Test_get_table_reservations(customer_token)
+        self.Test_success_view_table_reservations(waiter_token)
+        self.Test_success_update_customer_arrival(waiter_token, reservation_id)
+        self.Test_success_get_table_reservations(customer_token)
 
         # invalid data testing
         self.Test_invalid_update_customer_arrival(waiter_token, reservation_id + 1)
+
+        chef_data = {
+            "username": "chefTest",
+            "first_name": "Chef",
+            "last_name": "Test",
+            "email": "cheftest@gmail.com",
+            "password": "sep18g21",
+            "group": "Chef"
+        }
+        chef_token = self.register_user(chef_data, '/api/auth/staff/register_test')
+
+        receptionist_data = {
+            "username": "receptionistTest",
+            "first_name": "Receptionist",
+            "last_name": "Test",
+            "email": "receptionisttest@gmail.com",
+            "password": "sep18g21",
+            "group": "Receptionist"
+        }
+        receptionist_token = self.register_user(receptionist_data, '/api/auth/staff/register_test')
+
+        virtual_waiter_data = {
+            "username": "virtualWaiter",
+            "first_name": "",
+            "last_name": "",
+            "email": "",
+            "password": "sep18g21",
+            "group": "Virtual Waiter"
+        }
+        virtual_waiter_token = self.register_user(virtual_waiter_data, '/api/auth/staff/register_test')
+
+        # unauthorized access testing
+        self.Test_unauthorized_create_table_reservation(chef_token)
+        self.Test_unauthorized_create_table_reservation(waiter_token)
+        self.Test_unauthorized_create_table_reservation(receptionist_token)
+        self.Test_unauthorized_create_table_reservation(virtual_waiter_token)
+
+        self.Test_unauthorized_update_customer_arrival(chef_token, reservation_id)
+        self.Test_unauthorized_update_customer_arrival(receptionist_token, reservation_id)
+        self.Test_unauthorized_update_customer_arrival(customer_token, reservation_id)
+        self.Test_unauthorized_update_customer_arrival(virtual_waiter_token, reservation_id)
+
+        self.Test_unauthorized_get_table_reservations(chef_token)
+        self.Test_unauthorized_get_table_reservations(waiter_token)
+        self.Test_unauthorized_get_table_reservations(receptionist_token)
+        self.Test_unauthorized_get_table_reservations(virtual_waiter_token)
+
+        self.Test_unauthorized_view_table_reservations(chef_token)
+        self.Test_unauthorized_view_table_reservations(customer_token)
+        self.Test_unauthorized_view_table_reservations(receptionist_token)
+        self.Test_unauthorized_view_table_reservations(virtual_waiter_token)
+
+    def Test_create_table_reservation(self, token):
+        url = '/api/table_reservations'
+        data = {
+            "restaurant": RestaurantsTests().Test_create_restaurant(),
+            "meal_time": 2,
+            "reserved_date": str(datetime.now().date()),
+            "num_of_people": 4
+        }
+
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        response = client.post(url, data, format='json')
+        return response
+
+    def Test_update_customer_arrival(self, token, reservation_id):
+        url = '/api/table_reservations/arrival'
+        data = {
+            "reservation_id": reservation_id
+        }
+
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        response = client.post(url, data, format='json')
+        return response
+
+    def Test_view_table_reservations(self, token):
+        url = '/api/table_reservations/today_reservations'
+
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        response = client.get(url)
+        return response
+
+    def Test_get_table_reservations(self, token):
+        url = '/api/table_reservations'
+
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        response = client.get(url)
+        return response
 
     def register_user(self, data, url):
         client = Client()
