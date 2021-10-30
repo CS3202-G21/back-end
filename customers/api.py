bc1021e53383591d42c51069293bc665ed0b37ee
Dhaura Pathirana <dhaurapathirana@gmail.com>
2021-10-30 21:56:48 +0530

    customer profile picture and contact number added

--- customers/api.py
+++ customers/api.py
@@ -2,7 +2,9 @@ from rest_framework import generics, permissions, viewsets
 from rest_framework.response import Response
 from knox.models import AuthToken
 from django.contrib.auth.models import User
-from .serializers import CustomerSerializer, RegisterCustomerSerializer, LoginCustomerSerializer
+import json
+from .models import CustomerDetails
+from .serializers import CustomerSerializer, RegisterCustomerSerializer, LoginCustomerSerializer, CustomerDetailsSerializer
 
 
 # Register API
@@ -10,11 +12,22 @@ class RegisterCustomerAPI(generics.GenericAPIView):
     serializer_class = RegisterCustomerSerializer
 
     def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
+        data = json.loads(request.data['data'])
+        contact_no = data.pop('contact_no')
+
+        serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
         customer = serializer.save()
+        customer_details = CustomerDetails(customer_id=customer, contact_no=contact_no, profile_picture=request.FILES["profile_picture"])
+        customer_details.save()
+
+        user = CustomerSerializer(customer, context=self.get_serializer_context()).data
+        user_details = CustomerDetailsSerializer(customer_details, context=self.get_serializer_context()).data
+        user['contact_no'] = user_details['contact_no']
+        user['profile_picture'] = user_details['profile_picture']
+
         return Response({
-            "user": CustomerSerializer(customer, context=self.get_serializer_context()).data,
+            "user": user,
             "token": AuthToken.objects.create(customer)[1],
             "user_class": 0
         })
@@ -27,8 +40,16 @@ class LoginCustomerAPI(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         customer = serializer.validated_data
+
+        customer_details = CustomerDetails.objects.get(customer_id=customer.id)
+
+        user = CustomerSerializer(customer, context=self.get_serializer_context()).data
+        user_details = CustomerDetailsSerializer(customer_details, context=self.get_serializer_context()).data
+        user['contact_no'] = user_details['contact_no']
+        user['profile_picture'] = user_details['profile_picture']
+
         return Response({
-            "user": CustomerSerializer(customer, context=self.get_serializer_context()).data,
+            "user": user,
             "token": AuthToken.objects.create(customer)[1],
             "user_class": 0
         })
@@ -59,6 +80,11 @@ class GetUserFromIDAPI(generics.GenericAPIView):
         user_dict = user_info.__dict__
 
         user = {'id': user_dict['id'], 'username': user_dict['username'], 'first_name': user_dict['first_name'], 'last_name': user_dict['last_name'], 'email': user_dict['email']}
+        customer_details = CustomerDetails.objects.get(customer_id=user['id'])
+
+        user_details = CustomerDetailsSerializer(customer_details, context=self.get_serializer_context()).data
+        user['contact_no'] = user_details['contact_no']
+        user['profile_picture'] = user_details['profile_picture']
 
         return Response({
             "user": user,
--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class CustomerDetails(models.Model):
+    customer_id = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True)
+    contact_no = models.IntegerField(blank=True)
+    profile_picture = models.ImageField(upload_to='photos/customers', blank=True)
+
+    def __str__(self):
+        return str(self.id)
