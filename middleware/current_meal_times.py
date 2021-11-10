1fd170ea5cc4e497b2452c2b113cf8d373f5b93d
Dhaura Pathirana <57411348+dhaura@users.noreply.github.com>
2021-11-10 16:04:23 +0530

    Update current_meal_times.py

--- middleware/current_meal_times.py
+++ middleware/current_meal_times.py
@@ -3,9 +3,9 @@ from datetime import  datetime
 time_now = datetime.now().time()
 
 breakfast_begin = time_now.replace(hour=0, minute=1, second=0, microsecond=0)
-breakfast_end = time_now.replace(hour=10, minute=30, second=0, microsecond=0)
-lunch_begin = time_now.replace(hour=12, minute=0, second=0, microsecond=0)
-lunch_end = time_now.replace(hour=15, minute=30, second=0, microsecond=0)
+breakfast_end = time_now.replace(hour=13, minute=0, second=0, microsecond=0)
+lunch_begin = time_now.replace(hour=13, minute=0, second=0, microsecond=0)
+lunch_end = time_now.replace(hour=17, minute=30, second=0, microsecond=0)
 dinner_begin = time_now.replace(hour=17, minute=30, second=0, microsecond=0)
 dinner_end = time_now.replace(hour=23, minute=59, second=0, microsecond=0)
 
